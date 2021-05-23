aaca3fb8d7ad0b0c8fb11e0bc94403fa40ca497e
sayali-kadam <62012906+sayali-kadam@users.noreply.github.com>
2021-05-23 14:45:39 +0530

    Changed the path of the API

--- realTime.js
+++ realTime.js
@@ -6,13 +6,8 @@ Promise.all([
   //faceapi.nets.faceRecognitionNet.loadFromUri("/Face_recognition/models"),
   //faceapi.nets.faceExpressionNet.loadFromUri("/Face_recognition/models")
 
-  // faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
-  // faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
-  // faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
-  // faceapi.nets.faceExpressionNet.loadFromUri("./models")
-
   faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('./models')
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
   faceapi.nets.faceExpressionNet.loadFromUri('./models')
 
